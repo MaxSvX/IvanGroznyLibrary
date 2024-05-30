36d0059f5dd36c08ebf72bd5e97ab20142aca9fb
Max <max_sviridov@icloud.com>
2024-05-30 13:03:12 +0300

    Realizate to fun!

--- src/main.py
+++ src/main.py
@@ -12,13 +12,30 @@ class Book:
         return self.text
     
 def find_book_by_name(books:list[Book], name:str)->Book:
-    pass
+    for book in books:
+        if book.name == name:
+            return book
+def print_book(book:Book):
+    if book:
+        print((f"Автор: {book.author}\n" 
+                f"Название книги: {book.name}\n" 
+                f" Предисловие: {book.preface}\n" 
+                f"Текст: {book.text}\n" 
+                f"Возраст: {book.age_restrictrictions}\n" 
+                f"Рэйтинг: {book.rating}\n"))       
 def find_book_by_author(books:list[Book],authors:str)->list[Book]:
-    pass
-def sort_by_rating(books:list[Book], increse:bool = True)->list[Book]:
-    pass
+    massive = []
+    for book in books:
+        if book.author == authors:
+            massive.append(book)
+    return massive
+def sort_by_rating(books:list[Book], high_rating:float)->list[Book]:
+    if high_rating > Book:
+        print(Book)
 def main():
     array = [Book("Чехов","Лошадинная фамилия","","",12,4.8),
              Book("Эдвард Радзинский","Тайны фpанцузской империи","","",12,4.5),
-             Book("Лермантов","Странный человек","","",12,4.0)]
+             Book("Лермантов","Странный человек","","",12,4.0),
+             Book("Чехов", "Дядя Ваня","","",12,5.0)]
+    print_book(find_book_by_name(array,"Дядя Ваня"))        
 main()
